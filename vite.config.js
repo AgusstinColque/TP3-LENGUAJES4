19d91a94f5d53758a8c09e4f0cdf41c3b7e2f07f
AgustinColque <agustincolquevillalba@gmail.com>
2025-09-25 19:50:49 -0300

    Subida inicial git

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 Cambiá "mi-tp" por el nombre EXACTO de tu repo en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "/TP3-LENGUAJES4/", 
+  base: '/TP3-LENGUAJES4/',
 })
